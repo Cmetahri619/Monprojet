10b3e2018cfa87d5a5c728d0677ca8904487f7d3
celia.metahri@outlook.fr <celia.metahri@outlook.fr>
2026-07-31 16:06:46 +0200

    Modification du script Python

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
+
 print("Bonjour GitHub !")
+# *******************************************************
+# Nom ......... : script.py
+# Rôle ........ : Exemple simple pour l'exercice Git
+# Auteur ...... : Celia Metahri
+# Version ..... : V0.2
+# Licence ..... : Réalisé dans le cadre du cours IED
+# Usage ....... : python3 script.py
+# *******************************************************
+
+print("Bonjour GitHub !")
+print("Deuxième modification de mon projet Git."
